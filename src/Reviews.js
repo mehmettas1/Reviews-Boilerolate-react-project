c5347b2e5e9514f0bc6a673afa86c8dfa75d5de4
mehmet <tas349873@gmail.com>
2022-07-01 15:26:39 +0300

    Reviees-React

---
+++ src/Reviews.js
@@ -0,0 +1,12 @@
+import reviews from "./data"
+
+const Reviews = () => {
+  return (
+    <section>
+        <Reviews/>
+        <div className="btn-container"></div>
+    </section>
+  )
+}
+
+export default Reviews
